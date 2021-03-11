11c1adb004982b4f31b6ed98b96ce027144e84b1
glen <glen.sanjose@hotmail.ca>
2021-03-10 20:39:36 -0500

    h2 styling to fit better inside box

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ function Header(props) {
       <header>
          <div className="wrapper head">
             <h1>Motivation Board</h1>
-            <p>If you have any advice or quotes for your peers, leave them down below!</p>
+            <p>If you have any advice, something inspirational, or a quote for your peers, leave it down below!</p>
             { props.children }
          </div>
       </header>
